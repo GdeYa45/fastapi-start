399ad4a387e2fb25d5e01ff5ef322d53b9d35531
GdeYa45 <SpokiMokki@yandex.ru>
2026-07-08 16:16:15 +0300

    настройка pg, alembic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from src.api.hotels import router as router_hotels
 from src.database import *
+import sys
 
+sys.path.append(str(Path(__file__).parent.parent))
 app = FastAPI()
 
 app.include_router(router_hotels)
