9756963110110f3aa7aa9854d637427e96a8a610
GdeYa45 <SpokiMokki@yandex.ru>
2026-09-06 20:59:24 +0300

    creating a user model

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -13,6 +13,7 @@ from src.config import settings
 from src.database import Base
 from src.models.hotels import HotelsORM
 from src.models.rooms import RoomsORM
+from src.models.users import UsersORM
 from sqlalchemy.orm import DeclarativeBase
 
 # this is the Alembic Config object, which provides
@@ -31,24 +32,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
-
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -62,12 +46,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
