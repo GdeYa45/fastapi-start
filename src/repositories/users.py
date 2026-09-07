ad27a903f002d1cdbaa3be130af6abcd3b59e9df
GdeYa45 <SpokiMokki@yandex.ru>
2026-09-08 00:01:32 +0300

    create JWT token

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,3 +1,7 @@
+from pydantic.v1 import EmailStr
+from sqlalchemy import select
+
+from src.schemas.users import UserWithHashedPassword
 from src.repositories.base import BaseRepository
 from src.models.users import UsersORM
 from src.schemas.users import User
@@ -5,3 +9,11 @@ from src.schemas.users import User
 class UsersRepository(BaseRepository):
     model = UsersORM
     schema = User
+
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
+        return UserWithHashedPassword.model_validate(model)
